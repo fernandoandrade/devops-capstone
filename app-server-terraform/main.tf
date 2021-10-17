6f343827df6eeaa0c7f8f565ee717a64b38548d0
fernandoandrade <fcatavares@gmail.com>
2021-10-17 16:28:50 -0700

    Update main.tf

--- app-server-terraform/main.tf
+++ app-server-terraform/main.tf
@@ -10,8 +10,8 @@ resource "tls_private_key" "private-key" {
   rsa_bits    = 2048
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+resource "aws_key_pair" "app-server-key_pair" {
+  key_name   = "app-server-key"
   public_key = tls_private_key.private-key.public_key_openssh
 }
 
@@ -31,18 +31,18 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_security_group" "bsf-sg" {
-  name = "BSF-SG"
-  description = "BSF security group"
+resource "aws_security_group" "bsf-app-sg" {
+  name = "BSF-APP-SG"
+  description = "BSF App security group"
 
   tags = {
-    Name = "BSF-SG"
+    Name = "BSF-APP-SG"
     Environment = terraform.workspace
   }
 }
 
-resource "aws_security_group_rule" "create-sgr-ssh" {
-  security_group_id = aws_security_group.xyz-sg.id
+resource "aws_security_group_rule" "create-app-sgr-ssh" {
+  security_group_id = aws_security_group.bsf-app-sg.id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 22
   protocol          = "tcp"
@@ -50,8 +50,8 @@ resource "aws_security_group_rule" "create-sgr-ssh" {
   type              = "ingress"
 }
 
-resource "aws_security_group_rule" "create-sgr-inbound" {
-  security_group_id = aws_security_group.xyz-sg.id
+resource "aws_security_group_rule" "create-app-sgr-inbound" {
+  security_group_id = aws_security_group.bsf-app-sg.id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 0
   protocol          = "all"
@@ -59,8 +59,8 @@ resource "aws_security_group_rule" "create-sgr-inbound" {
   type              = "ingress"
 }
 
-resource "aws_security_group_rule" "create-sgr-outbound" {
-  security_group_id = aws_security_group.xyz-sg.id
+resource "aws_security_group_rule" "create-app-sgr-outbound" {
+  security_group_id = aws_security_group.bsf-app-sg.id
   cidr_blocks         = ["0.0.0.0/0"]
   from_port         = 0
   protocol          = "all"
@@ -68,24 +68,24 @@ resource "aws_security_group_rule" "create-sgr-outbound" {
   type              = "egress"
 }
 
-resource "aws_instance" "jenkins" {
-  count         = 1
+resource "aws_instance" "app-servers" {
+  count         = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
-  security_groups = ["BSF-SG"]
+  key_name      = aws_key_pair.app-server-key_pair.key_name
+  security_groups = ["BSF-APP-SG"]
   tags = {
-    Name = "bsf-${count.index}"
+    Name = "bsf-app-${count.index}"
   }
 }
 
-resource "null_resource" "deploy-server" {
+resource "null_resource" "app-server" {
     
     connection {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.private-key.private_key_pem
-      host        = aws_instance.jenkins.*.public_dns[0]
+      host        = aws_instance.app-servers.*.public_dns[0]
     }
 
     provisioner "remote-exec" {
