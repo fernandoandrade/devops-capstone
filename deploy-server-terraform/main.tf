3f26e05f0f42599ebcac30a8e4364dddcf7e8f39
fernandoandrade <fcatavares@gmail.com>
2021-10-17 17:10:11 -0700

    Update main.tf

--- deploy-server-terraform/main.tf
+++ deploy-server-terraform/main.tf
@@ -68,8 +68,7 @@ resource "aws_security_group_rule" "create-infra-sgr-outbound" {
   type              = "egress"
 }
 
-resource "aws_instance" "infra-servers" {
-  count         = 2
+resource "aws_instance" "infra-server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.infra-server-key_pair.key_name
@@ -77,60 +76,50 @@ resource "aws_instance" "infra-servers" {
   tags = {
     Name = "bsf-infra-${count.index}"
   }
-}
-
-resource "null_resource" "infra-server" {
-    
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.private-key.private_key_pem
-      host        = aws_instance.infra-servers.*.public_dns[0]
-    }
-
-    provisioner "file" {
-      source      = "variables.tf"
-      destination = "./variables.tf"
-    }
   
-    provisioner "remote-exec" {
-      inline = [
-        "sudo apt update ",
-        "echo Ansible",
-        "sudo apt install -y software-properties-common ",
-        "sudo add-apt-repository --yes --update ppa:ansible/ansible ",
-        "sudo apt install ansible -y ",
-        "echo '[webservers]' > ~/hosts",
-        "echo '${aws_instance.web.*.public_dns[1]}' >> ~/hosts",
-        "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/bsafe.pem && chmod 600 ~/.ssh/bsafe.pem",
-        "sudo sed -i '71s/.*/host_key_checking = False/' /etc/ansible/ansible.cfg",
-        "echo Install Java",
-        "sudo apt install -y openjdk-11-jdk ",
-        "sudo apt-get install -y git",
-        "echo Install Maven",
-        "sudo apt-get install -y maven",
-        "echo Install Docker",
-        "sudo apt-get update",
-        "sudo apt-get remove docker docker-engine docker.io",
-        "sudo apt install docker.io",
-        "sudo systemctl start docker",
-        "sudo systemctl enable docker",
-        "echo Install Jenkins",
-        "sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-        "sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-        "sudo apt update",
-        "sudo apt install -y jenkins",
-        "sudo systemctl start jenkins",
-        "echo 'Java_Home:'",
-        "readlink -f $(which java)",
-        "echo 'Mvn_Home:'",
-        "mvn -v",
-        "echo 'Jenkins Initial Admin password:'",
-        "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
-      ]
-    }
+  provisioner "file" {
+    source      = "variables.tf"
+    destination = "./variables.tf"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update ",
+      "echo Ansible",
+      "sudo apt install -y software-properties-common ",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible ",
+      "sudo apt install ansible -y ",
+      "echo '[webservers]' > ~/hosts",
+      "echo '${aws_instance.web.*.public_dns[1]}' >> ~/hosts",
+      "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/bsafe.pem && chmod 600 ~/.ssh/bsafe.pem",
+      "sudo sed -i '71s/.*/host_key_checking = False/' /etc/ansible/ansible.cfg",
+      "echo Install Java",
+      "sudo apt install -y openjdk-11-jdk ",
+      "sudo apt-get install -y git",
+      "echo Install Maven",
+      "sudo apt-get install -y maven",
+      "echo Install Docker",
+      "sudo apt-get update",
+      "sudo apt-get remove docker docker-engine docker.io",
+      "sudo apt install docker.io",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "echo Install Jenkins",
+      "sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "echo 'Java_Home:'",
+      "readlink -f $(which java)",
+      "echo 'Mvn_Home:'",
+      "mvn -v",
+      "echo 'Jenkins Initial Admin password:'",
+      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+    ]
+  }
 
-    provisioner "local-exec" {
-      command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/bsafe.pem && chmod 600 ~/.ssh/bsafe.pem "
-    }
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/bsafe.pem && chmod 600 ~/.ssh/bsafe.pem "
+  }
 }
